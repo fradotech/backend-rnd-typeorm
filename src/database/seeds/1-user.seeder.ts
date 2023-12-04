4d7c220a02fbbf88aa6c88785e16f6990d73c45d
fradotech <fradotech.id@gmail.com>
2023-12-04 09:40:48 +0700

    refactor(user): seeder pecah ke function

--- src/database/seeds/1-user.seeder.ts
+++ src/database/seeds/1-user.seeder.ts
@@ -4,21 +4,29 @@ import { randFullName } from '@ngneat/falso';
 import { createEntityManager } from '../entity-manager';
 
 export default class CreateUsers implements Seeder {
-  public async run(): Promise<any> {
+  public async run(): Promise<void> {
     const entityManager = await createEntityManager();
-    const users = [];
 
-    for (let i = 0; i < 1000; i++) {
+    const users = await this.createMany();
+
+    await entityManager
+      .createQueryBuilder(User, 'users')
+      .insert()
+      .values(users)
+      .execute();
+  }
+
+  // Utils
+
+  private async createMany(count = 1000): Promise<User[]> {
+    const users: User[] = [];
+
+    for (let i = 0; i < count; i++) {
       const user = new User();
       user.name = randFullName();
       users.push(user);
     }
 
-    await entityManager
-      .createQueryBuilder(User, 'users')
-      .insert()
-      .into(User)
-      .values(users)
-      .execute();
+    return users;
   }
 }
